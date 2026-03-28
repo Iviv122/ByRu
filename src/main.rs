3d78b765bea79de0c555ac04a8beccfef05fae98
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2026-03-28 12:51:35 +0100

    works??

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{thread::sleep, time::Duration, vec};
+use std::{panic::PanicHookInfo, thread::sleep, time::Duration, vec};
 
 use minifb::{Key, Window, WindowOptions};
 
@@ -6,11 +6,14 @@ const MEMORY_SIZE: usize = 0x1000008;
 
 const WIDTH: usize = 256;
 const HEIGHT: usize = 256;
+const VIDBUFFSIZE : usize = 256*256;
 
 const RAMETIME: Duration = Duration::from_micros(16_667);
 
 const COLORAMOUNT: usize = 256;
 const COLORSTEP: usize = 0x33;
+
+const INTSIZE: usize = 3; // 3 bytes for int
 
 fn init_color_map(map: &mut Vec<u32>) -> () {
     let mut index = 0;
@@ -25,15 +28,44 @@ fn init_color_map(map: &mut Vec<u32>) -> () {
     }
 }
 
+fn slice_to_u32(bytes: &[u8]) -> u32 {
+    // big indian
+    let ret: u32 = ((bytes[0] as u32) << 16) | ((bytes[1] as u32) << 8) | (bytes[2] as u32);
+    return ret;
+}
+
+fn update(mem: &mut Vec<u8>) -> () {
+    let mut pc: usize = slice_to_u32(&mem[2..2 + INTSIZE]) as usize;
+
+    for _ in 1..=65536 {
+        let adr_a: usize = slice_to_u32(&mem[pc..pc + INTSIZE]) as usize;
+        let adr_b: usize = slice_to_u32(&mem[pc + INTSIZE..pc + INTSIZE * 2]) as usize;
+        let adr_c: usize = slice_to_u32(&mem[pc + INTSIZE * 2..pc + INTSIZE * 3]) as usize;
+
+        mem[adr_a] = mem[adr_b];
+        pc = adr_c;
+    }
+}
+
+fn draw(mem: &mut Vec<u8>,vid: &mut Vec<u32>,colors: &Vec<u32>) -> () {
+    let pixels_addr: usize = ((mem[5] as u32) << 24) as usize;
+    
+    for i in 0..=VIDBUFFSIZE-1{
+        let y = i / WIDTH;
+        let x = i % WIDTH;
+        let color_index = slice_to_u32(&mem[pixels_addr+i..pixels_addr+i+INTSIZE]) as usize;
+        vid[WIDTH*y+x] = colors[color_index];
+    }
+}
 fn main() {
-    let mut memory: Vec<u32> = vec![0; MEMORY_SIZE];
+    let mut memory: Vec<u8> = vec![0; MEMORY_SIZE];
     let mut video_buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
     let mut color_map: Vec<u32> = vec![0; COLORAMOUNT];
 
     init_color_map(&mut color_map);
 
     let mut window = Window::new(
-        "Test - ESC to exit",
+        "BytePusher - ESC to exit",
         WIDTH,
         HEIGHT,
         WindowOptions::default(),
@@ -51,8 +83,8 @@ fn main() {
     }
     while window.is_open() && !window.is_key_down(Key::Escape) {
         sleep(RAMETIME);
-        // TODO: Keyboard
-
+        update(&mut memory);
+        draw(&mut memory,&mut video_buffer,&color_map);
         window
             .update_with_buffer(&video_buffer, WIDTH, HEIGHT)
             .unwrap();
